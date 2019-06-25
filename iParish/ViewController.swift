3a136f37fb817f6eba58a1db21d8b2919cbfa2d1
Valentine Ofili <valentine.ofili@gmail.com>
2019-06-25 11:55:07 +0100

    Initial Commit

---
+++ iParish/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iParish
+//
+//  Created by Valentine Ofili on 25/06/2019.
+//  Copyright © 2019 Valentine Ofili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
